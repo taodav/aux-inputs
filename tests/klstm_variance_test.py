fix klstm variance test

tests/klstm_variance_test.py
@@ -1,8 +1,9 @@
 import numpy as np
-import gym
 import optax
+import dill
 from jax import random
-from typing import Any, Tuple
+from pathlib import Path
+from typing import Tuple
 
 from unc.envs import get_env, Environment
 from unc.args import Args
@@ -20,7 +21,7 @@ def hs_stats(agent: kLSTMAgent) -> Tuple[np.ndarray, float, np.ndarray, float]:
     """
     lstm_state = agent.state
 
-    hs, cs = lstm_state[:, 0], lstm_state[:, 1]
+    hs, cs = lstm_state[0], lstm_state[1]
     mean_hs, mean_cs = hs.mean(axis=0), cs.mean(axis=0)
     hs_var, cs_var = ((hs - mean_hs) ** 2).mean(axis=0), ((cs - mean_cs) ** 2).mean(axis=0)
     return hs_var, hs_var.mean(axis=0), cs_var, cs_var.mean(axis=0)
@@ -32,15 +33,30 @@ def pf_stats(env: Environment):
 
 
 if __name__ == "__main__":
+    # Log info every 5 episodes
+    log_episode_freq = 5
+    all_info = {
+        'pf_vars': [],
+        'hs_vars': [],
+        'cs_vars': [],
+        'ep_end_timesteps': []
+    }
+    info_path = Path('klstm_variance_results.pkl')
+
     parser = Args()
     args = parser.parse_args()
-    args.n_hidden = 10
+    args.n_hidden = 20
     args.trunc = 10
     args.epsilon = 0.1
     args.seed = 2020
     args.total_steps = int(1e6)
     args.k_rnn_hs = 10
     args.action_cond = "cat"
+    args.same_k_rnn_params = True
+    args.replay = True
+    args.step_size = 0.0001
+    args.value_step_size = 0.00025
+    args.init_hidden_var = 0.5
 
     # Compass World variables
     args.env = "fp"
@@ -56,16 +72,16 @@ if __name__ == "__main__":
 
     network = build_network(args.n_hidden, train_env.action_space.n, model_str="lstm")
     optimizer = optax.adam(args.step_size)
-    value_network = build_network(args.n_hidden, train_env.action_space.n, model_str="nn")
-    value_optimizer = optax.adam(args.value_step_size)
 
-    n_features = train_env.observation_space.shape[0]
+    n_features, n_actions = train_env.observation_space.shape[0], train_env.action_space.n
     if args.action_cond == 'cat':
-        n_features += train_env.action_space.n
+        n_features += n_actions
 
+    value_network = build_network(args.n_hidden, train_env.action_space.n, model_str="seq_value")
+    value_optimizer = optax.adam(args.value_step_size)
     agent = kLSTMAgent(network, value_network, optimizer, value_optimizer,
-                       n_features,
-                       train_env.action_space.n, rand_key, args)
+                       n_features, n_actions, rand_key, args)
+
     agent.set_eps(args.epsilon)
 
     buffer = EpisodeBuffer(args.buffer_size, train_env.rng, (n_features,),
@@ -76,6 +92,10 @@ if __name__ == "__main__":
 
     eps = 0
     while steps < args.total_steps:
+
+        ep_pf_vars = []
+        ep_hs_vars = []
+        ep_cs_vars = []
 
         obs = train_env.reset()
         if args.action_cond == "cat":
@@ -122,9 +142,13 @@ if __name__ == "__main__":
                 batch.gamma = (1 - batch.done) * args.discounting
                 loss = agent.update(batch)
 
+            hs_var, hs_avg_var, cs_var, cs_avg_var = hs_stats(agent)
+            pf_mean, pf_var = pf_stats(train_env)
+            ep_pf_vars.append(pf_var)
+            ep_hs_vars.append(hs_var)
+            ep_cs_vars.append(cs_var)
+
             if steps % 10 == 0:
-                hs_var, hs_avg_var, cs_var, cs_avg_var = hs_stats(agent)
-                pf_mean, pf_var = pf_stats(train_env)
 
                 print(f"Total steps {steps}, "
                       f"Episode {eps}, "
@@ -142,9 +166,16 @@ if __name__ == "__main__":
             hs = next_hs
 
         eps += 1
-        print()
-        print("END Of EPISODE")
+        if eps % log_episode_freq == 0:
+            all_info['pf_vars'].append(ep_pf_vars)
+            all_info['hs_vars'].append(ep_hs_vars)
+            all_info['cs_vars'].append(ep_cs_vars)
+            all_info['ep_end_timesteps'].append(steps)
 
+        print(f"End of episode {eps}")
         print()
+
+    with open(info_path, "wb") as f:
+        dill.dump(all_info, f)
 
 